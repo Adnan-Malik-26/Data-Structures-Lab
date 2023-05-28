37cd578f3299c0ae2b3cefc844436042402d4394
adnan-malik <eddymalikadnan@gmail.com>
2023-05-28 14:56:58 +0530

    My Changes

--- queue.c
+++ queue.c
@@ -33,9 +33,9 @@ main()
             exit(1);
             default:
             printf("Wrong choice \n");
-        } /* End of switch */
-    } /* End of while */
-} /* End of main() */
+        } 
+    } 
+} 
 
 void insert()
 {
@@ -45,14 +45,13 @@ void insert()
     else
     {
         if (front == - 1)
-        /*If queue is initially empty */
         front = 0;
         printf("Inset the element in queue : ");
         scanf("%d", &add_item);
         rear = rear + 1;
         queue_array[rear] = add_item;
     }
-} /* End of insert() */
+} 
 
 void delete()
 {
@@ -66,7 +65,7 @@ void delete()
         printf("Element deleted from queue is : %d\n", queue_array[front]);
         front = front + 1;
     }
-} /* End of delete() */
+} 
 
 void display()
 {
@@ -80,4 +79,4 @@ void display()
             printf("%d ", queue_array[i]);
         printf("\n");
     }
-} /* End of display() */
+} 
